634c0dc7b0a6b1a73109a9f4a16720583554c115
asmaaEzz <asmaaezzeldean1@gmail.com>
2019-02-03 21:34:14 +0200

    upload project

---
+++ app/src/main/java/com/example/asmaa/gitrx/GitHubService.java
@@ -0,0 +1,14 @@
+package com.example.asmaa.gitrx;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GitHubService {
+    @GET("users/{user}/repos")
+    Observable<List<GitHubRepo>> getStarredRepositories(@Path("user") String userName);
+}
+
+
